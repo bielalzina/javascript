f093d2ed32f72fd35c173188d2bbea59c17dd26d
bielalzina <bielalzina@cifpjoantaix.cat>
2021-11-07 11:15:20 +0100

    FINS 13

--- UT2_ACTIVITATS_APRENENTATGE/js/utilitats.js
+++ UT2_ACTIVITATS_APRENENTATGE/js/utilitats.js
@@ -67,3 +67,79 @@ function validaDecimal(numero) {
         return false;
     }
 }
+
+/** Demana a l'usuari un nombre sencer. Mostra el missatge a l'alert per informar
+ * a l'usuari de quina dada li demanam. No accepta cap dada que no sigui un sencer.
+ *
+ * @param  cadena El prompt de l'alert.
+ * @returns un valor sencer
+ */
+
+
+function llegeixSencer(missatge) {
+    let numero;
+    do {
+        numero = prompt(missatge);
+    } while (!validaSencer(numero));
+    return parseInt(numero);
+}
+
+/**
+ * Comprova que un camp obligatori contengui informació. Realitza tres comprovacions:
+ * <ol>
+ * <li> El valor no pot ser null</li>
+ * <li> El valor no pot ser undefined</li>
+ * <li> El valor no pot contenir únicament espais en blanc.</li>
+ *
+ * </ol>
+
+ * @param {type} valor El valor que comprovam
+ * @returns {Boolean} true si conte dades, false en cas contrari*/
+
+
+function validaObligatori(valor) {
+    if (valor !== null & valor !== 'undefined') {
+        // Convertim valor a cadena i eliminam espais en blanc al principi i al final
+        // Evitam que un usuari només introdueixi espais
+        valor = valor.toString().trim()
+        if (valor.length > 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/** Comprova que el paràmetre telefon contengui un telèfon espanyol vàlid.
+ * Si especificam el segon  paràmetre podem afinar la validació a un mòbil o fixe.
+ *
+ * @param telefon El nombre de telefon a validar.
+ * @param tipus 0 => mòbil 1 => fixe undefined => qualsevol
+ * @returns true si el telèfon és correcte.
+ */
+function validarTelefon(telefon, tipus) {
+    if (validarObligatori(telefon) && !isNaN(telefon)) {
+        telefon = telefon + "";
+        if (validarSencer(telefon) && (telefon.length === 9)) {
+            if (((telefon.charAt(0) === "6") || (telefon.charAt(0) === "7"))
+                && ((typeof tipus === 'undefined') || (tipus === 0))) {
+                return true;
+            }
+            if (((telefon.charAt(0) === "8") || (telefon.charAt(0) === "9"))
+                && ((typeof tipus === 'undefined') || (tipus === 1))) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function validaTelefonMobil(telefon, tipus) {
+    if (validaObligatori(telefon) && !isNaN(telefon)) {
+        if (validaSencer(telefon) && (telefon.length === 9)) {
+            if (tipus === "M" && ((telefon.charAt(0) === "6") || (telefon.charAt(0) === "7"))) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
